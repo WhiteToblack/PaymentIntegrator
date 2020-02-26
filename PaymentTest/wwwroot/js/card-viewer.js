e399b72b2a10cfc01a87977d1ca80035b915fce1
WhiteToBlack <mertc90@gmail.com>
2020-02-27 01:18:55 +0300

    Card Component Input Selectors changed

--- PaymentTest/wwwroot/js/card-viewer.js
+++ PaymentTest/wwwroot/js/card-viewer.js
@@ -1,10 +1,16 @@
-﻿$('Form').card({   
-    container: '.card-wrapper', 
+﻿$('Form').card({
+    container: '.card-wrapper',
     placeholders: {
         number: '**** **** **** ****',
         name: 'Name Surname',
         expiry: '**/****',
         cvc: '***',
-        form:'CardForm'
+        form: 'CardForm'
+    },
+    formSelectors: {
+        numberInput: 'input[name="CardInformation.CardNumber"]',
+        expiryInput: 'input[name="CardInformation.ExpiryDate"]',
+        cvcInput: 'input[name="CardInformation.Cvc"]',
+        nameInput: 'input[name="User.NameSurname"]'
     }
 });
